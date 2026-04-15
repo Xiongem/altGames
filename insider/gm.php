8200dd1d428635c776267aea6dd95ea3ddb0a63b
Jamie Work <earthlove97@gmail.com>
2026-04-15 09:57:02 +0900

    fixed favicon link

--- insider/gm.php
+++ insider/gm.php
@@ -15,7 +15,7 @@ echo $players;?>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/gm.css">
-    <link rel="website icon" type="webp" href="../images/favicon.png">
+    <link rel="website icon" type="webp" href="../images/favicon.webp">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="js/scripts.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
--- insider/start.php
+++ insider/start.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/start.css">
-    <link rel="website icon" type="webp" href="../images/favicon.png">
+    <link rel="website icon" type="webp" href="../images/favicon.webp">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="js/scripts.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
